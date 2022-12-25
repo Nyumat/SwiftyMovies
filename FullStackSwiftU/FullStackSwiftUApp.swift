f1375cc8dce12b6e5f5ce50c13bd46428f515f22
Thomas <nyumat18@gmail.com>
2022-12-25 04:44:24 -0800

    Initial Commit

---
+++ FullStackSwiftU/FullStackSwiftUApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullStackSwiftUApp.swift
+//  FullStackSwiftU
+//
+//  Created by Thomas Nyuma on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct FullStackSwiftUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
